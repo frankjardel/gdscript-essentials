2fc47fa47edc1e46da32d53e33b4e49cd56a6912
frankjardel <jardelfrank42@gmail.com>
2025-09-05 17:58:36 -0300

    finalize Functions Math

--- scripts/FuncMath.gd
+++ scripts/FuncMath.gd
@@ -15,3 +15,12 @@ func _ready():
 	print(sqrt(25)) # Retorna a raiz quadrada. 
 	print(pow(2, 3)) # Retorna o valor elevado à potência. 2 * 2 * 2 = 8
 	print(round(2.9)) # Retorna o valor arredondado para o inteiro mais próximo.
+	
+	print(min(n1, n2))
+	print(max(n1, n2))
+	
+	print(randi()) # entre 0 e 4.294.967.295
+	print(randi() % 50) # entre 0 e 49
+	print(randi() % 51 + 10) # entre 10 e 60
+	
+	print(randf()) # retorna um float randomico
