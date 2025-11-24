70bee5ae86f9b27ede33a932b43f319d29932004
frankjardel <jardelfrank42@gmail.com>
2025-11-24 16:57:34 -0300

    #comments..

---
+++ scripts/Comments.gd
@@ -0,0 +1,47 @@
+"""
+Autor: frankjardel
+Email: underdogzdev@gmail.com
+
+Licença: MIT
+Versão: 1.0
+"""
+extends Control
+
+
+func _ready() -> void:
+	pass # pass = passe por enquanto..
+"""
+Melhoram a compreensão:
+	Tornam o código mais legível para outros programadores, especialmente em
+	equipes de desenvolvimento.
+Documentam Decisões:
+	Explicam o porquê de certas abordagens, o que pode ser crucial quando o
+	código precisa ser mantido no futuro por pessoas que não conhecem o contexto
+	atual.
+Explicam lógicas complexas:
+	Ajudam a desmistificar partes complicadas do código, tornando-as mais fáceis
+	de seguir.
+Justificam "Soluções estranhas":
+	Podem explicar por que uma ação que parece incomum foi tomada, como ignorar 
+	um erro específico ou usar um determinado método.
+"""
+
+"""
+Melhores Práticas
+Seja conciso:
+	Comentários devem ser breves e diretos, focando na intenção ou na razão por
+	trás do código, não na descrição de cada linha.
+Mantenha-os atualizados:
+	Se o código mudar, o comentário também deve ser atualizado. Comentários
+	desatualizados podem causar mais confusão do que ajudar.
+Evite o óbvio:
+	Não comente o que o próprio código já mostra claramente. Um comentário deve
+	adicionar valor, explicando o "porquê" e não o "o quê".
+Use para explicações complexas:
+	Se um trecho de código for excessivamente complexo, considere reestruturá-lo
+	(refatorar) para que se torne mais autoexplicativo. Se ainda assim for necessário
+	use um comentário.
+Use ferramentas adequadas:
+	Para tarefas e gerenciamento de bugs, utilize ferramentas apropriadas (como GIT)
+	em vez de comentários, que podem se tornar dívida técnica.
+"""
--- scripts/operators_relational.gd
+++ scripts/operators_relational.gd
@@ -1,1 +1,18 @@
 extends Control
+
+var health = 100
+
+func _process(delta):
+	if health == 100:
+		print("estou muito bem obrigado!")
+	if health > 0:
+		print("ainda vivo..")
+	if health >= 1:
+		print("ainda está vivo")
+	if health < 0:
+		print("game over")
+	if health <= 0:
+		print("game over")
+		
+	if health != 0:
+		print("ainda está vivo")
